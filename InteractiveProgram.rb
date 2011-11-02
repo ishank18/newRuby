73dbd21d49be971fbafe11d25ad794b39502369a
Ishank Gupta <ishank.gupta@vinsol.com>
2011-11-02 17:10:15 +0530

    Interactive program

---
+++ InteractiveProgram.rb
@@ -0,0 +1,12 @@
+prog_array = [""] #Array that will take input
+while(prog_array[prog_array.length - 1].chomp != "q")
+	prog_array[prog_array.length] = gets
+end
+prog_array.pop
+prog_string = ""
+prog_array.each do |str|  #Converts the array into string
+	prog_string += str
+end
+puts "\n\nOutput of program is : \n\n"
+eval prog_string 
+puts "\n"
