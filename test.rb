2fb88e40e276b8ccd6ecd79bbbeeff95c4d860e1
Ishank Gupta <ishank.gupta@vinsol.com>
2011-11-03 17:24:57 +0530

    Program from command line

--- test.rb
+++ test.rb
@@ -1,6 +1,25 @@
-def ishank
-	var = 5
-	binding
+module CreateAccessor
+	def setter_getter(name_of_var)
+		define_method("#{name_of_var}=") do |val|
+			eval "@#{name_of_var} = val" 
+		end
+		define_method("#{name_of_var}") do
+			eval "@#{name_of_var}" 
+		end
+	end
 end
-r = ishank	
-eval "puts var", r
+class Test
+	extend CreateAccessor
+	setter_getter :a
+	def initialize(a, b)
+		@a = a
+		@b = b
+	end
+	def sum
+		puts @a + @b
+	end
+end
+t = Test.new(2,6)
+t.a = 4
+puts t.a
+t.sum
