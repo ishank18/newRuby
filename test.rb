c647f4e794c6edd329136e423467c0707739111e
Ishank Gupta <ishank.gupta@vinsol.com>
2011-11-04 17:08:35 +0530

    CSV to CLass

--- test.rb
+++ test.rb
@@ -1,25 +1,7 @@
-module CreateAccessor
-	def setter_getter(name_of_var)
-		define_method("#{name_of_var}=") do |val|
-			eval "@#{name_of_var} = val" 
-		end
-		define_method("#{name_of_var}") do
-			eval "@#{name_of_var}" 
-		end
-	end
+class Ishank
+define_method("ishank") do
+	puts "Method called!"
 end
-class Test
-	extend CreateAccessor
-	setter_getter :a
-	def initialize(a, b)
-		@a = a
-		@b = b
-	end
-	def sum
-		puts @a + @b
-	end
 end
-t = Test.new(2,6)
-t.a = 4
-puts t.a
-t.sum
+i = Ishank.new
+i.ishank
